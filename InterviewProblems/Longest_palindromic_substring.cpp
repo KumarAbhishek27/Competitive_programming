46106849d2fd76cff208a3da5d4e0c8adc62727b
KumarAbhishek27 <31176318+KumarAbhishek27@users.noreply.github.com>
2020-07-14 19:53:53 +0530

    Update Longest_palindromic_substring.cpp

--- InterviewProblems/Longest_palindromic_substring.cpp
+++ InterviewProblems/Longest_palindromic_substring.cpp
@@ -81,6 +81,63 @@ string longestPalindromicSubstring(string str){
     return str.substr(start, maxLen);
 }
 
+// ==================================================================
+/* Method 2*/
+// Time Complexity: O(n^2)
+// Space Complexity: O(1)
+
+void printSubStr(char* str, int low, int high){ 
+    for (int i = low; i <= high; ++i) 
+        cout << str[i]; 
+} 
+
+int longestPalSubstr(char* str){ 
+    // The result (length of LPS) 
+    int maxLength = 1; 
+  
+    int start = 0; 
+    int len = strlen(str); 
+  
+    int low, high; 
+  
+    // One by one consider every 
+    // character as center point of 
+    // even and length palindromes 
+    for (int i = 1; i < len; ++i) { 
+        // Find the longest even length palindrome 
+        // with center points as i-1 and i. 
+        low = i - 1; 
+        high = i; 
+        while (low >= 0 && high < len && str[low] == str[high]) { 
+            if (high - low + 1 > maxLength) { 
+                start = low; 
+                maxLength = high - low + 1; 
+            } 
+            --low; 
+            ++high; 
+        } 
+  
+        // Find the longest odd length 
+        // palindrome with center point as i 
+        low = i - 1; 
+        high = i + 1; 
+        while (low >= 0 && high < len && str[low] == str[high]) { 
+            if (high - low + 1 > maxLength) { 
+                start = low; 
+                maxLength = high - low + 1; 
+            } 
+            --low; 
+            ++high; 
+        } 
+    } 
+  
+    cout << "Longest palindrome substring is: "; 
+    printSubStr(str, start, start + maxLength - 1); 
+  
+    return maxLength; 
+} 
+
+
 int32_t main() {
 
     ios_base::sync_with_stdio(false);
